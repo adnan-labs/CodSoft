76a00a37de083c8e8ae9578b45bfdcbe4c626c7f
Adnan <ashaikh7187@gmail.com>
2026-03-30 16:29:12 +0530

    task3 completed

---
+++ Task 3/script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let expression = display.value;
+
+    // convert % → /100
+    expression = expression.replace(/%/g, "/100");
+
+    display.value = eval(expression);
+  } catch {
+    display.value = "Error";
+  }
+}
